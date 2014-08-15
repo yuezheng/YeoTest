904ffad68cb4fca963b1eb751e316402ad894b95
Zheng Yue <zhengyue@gohighedu.com>
2014-08-15 13:47:31 +0800

    Add highcharts to libiary
    1. Add package highcharts to draw charts;
    2. Add a angular service.

---
+++ app/scripts/services.js
@@ -0,0 +1,13 @@
+'use strice';
+
+/* Services */
+
+angular.module('Test.services', []).
+    value('version', '0.1').
+    service('messageService', ['$root', function ($rootScope) {
+        return {
+            change: function(value) {
+                $rootScope.$broadcast('messageService.update', value);
+            }
+        }        
+    }]).service();
